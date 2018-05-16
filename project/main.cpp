fcb21aae42acc16000a3a5471ecd415bd3204edc
SAKryukov <github@sakryukov.org>
2018-05-16 07:53:35 -0400

    Remote GDB

---
+++ project/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "1" << std::endl;
+    std::cout << "2" << std::endl;
+    std::cout << "3" << std::endl;
+    return 0;
+}
