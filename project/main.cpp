1739a0f620c0ecf688ae073140c1e60041649f0f
SAKryukov <github@sakryukov.org>
2018-06-06 08:38:22 -0400

    Changed code to remove OS-specific

--- project/main.cpp
+++ project/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
 int main() {
+    int x = 11;
+    int y = 12;
+    int z = x * y;
+    /*
     std::cout << "Hello, World!" << std::endl;
     std::cout << "1" << std::endl;
     std::cout << "2" << std::endl;
     std::cout << "3" << std::endl;
     std::cout << sizeof(void*) << std::endl;
+    */
     return 0;
 }
