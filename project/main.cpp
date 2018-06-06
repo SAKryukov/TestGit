edef4d06d5a666629703e88526c66db06461d483
SAKryukov <github@sakryukov.org>
2018-06-06 11:14:17 -0400

    Restored I/O in sample

--- project/main.cpp
+++ project/main.cpp
@@ -4,12 +4,10 @@ int main() {
     int x = 11;
     int y = 12;
     int z = x * y;
-    /*
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "1" << std::endl;
-    std::cout << "2" << std::endl;
-    std::cout << "3" << std::endl;
-    std::cout << sizeof(void*) << std::endl;
-    */
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
+    std::cout << z << std::endl;
+    std::cout << "Size of pointer: " << 8 * sizeof(void*) << std::endl;
     return 0;
 }
